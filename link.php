abe7ec95af653d6ee558dd62f77feaa03b03c994
edisonquispe <edisonquispevargas@gmail.com>
2020-08-18 20:12:44 -0500

    nuevo php

---
+++ link.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+   <script src="js/sweet-alert.min.js"></script>
+    <link rel="stylesheet" href="css/material-design-iconic-font.min.css">
+    <link rel="Shortcut Icon" type="image/x-icon" href="assets/icons/icon.jpg" />
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/jquery.mCustomScrollbar.css">
+    <link rel="stylesheet" href="css/style.css">
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+    <script>window.jQuery || document.write('<script src="js/jquery-1.11.2.min.js"><\/script>')</script>
+    <script src="js/modernizr.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/jquery.mCustomScrollbar.concat.min.js"></script>
+    <script src="js/main.js"></script>
+</head>
+<body>
+    
+</body>
+</html>
