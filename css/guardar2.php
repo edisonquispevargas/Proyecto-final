9eea9ba3dd369033f2fe3508e8bf961442834c3e
edisonquispe <edisonquispevargas@gmail.com>
2020-08-23 20:46:46 -0500

    nuevo php

---
+++ css/guardar2.php
@@ -0,0 +1,100 @@
+<?php
+include('link.php');
+require '../conexion.php';
+
+$nombre = $_POST['nombre'];
+$cantidad = $_POST['cantidad'];
+$precio = $_POST['precio'];
+$fecha = $_POST['fecha'];
+$proveedor = $_POST['proveedor'];
+$desc = $_POST['descripcion'];
+
+$sql = "INSERT INTO productos(nombre_producto,cantidad,precio_total,fecha_entrada,proveedor,descripcion) 
+    VALUES ('$nombre','$cantidad','$precio','$fecha','$proveedor','$desc')";
+    
+    $resultado = $mysqli->query($sql);
+?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <title>Guardado Producto con Exito</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+   
+    
+    
+</head>
+<body>
+    <div class="navbar-lateral full-reset">
+        <div class="visible-xs font-movile-menu mobile-menu-button"></div>
+        <div class="full-reset container-menu-movile custom-scroll-containers">
+            <div class="logo ">
+                <i class="visible-xs zmdi zmdi-close pull-left mobile-menu-button" 
+                style="line-height: 55px; 
+                       cursor: pointer; 
+                       padding: 0 10px; 
+                       margin-left: 7px;">
+                </i> 
+                Sistema de Almacen
+            </div>
+            <div>
+                <figure>
+                    <img src="../assets/img/logo.jpg" alt="Biblioteca" class="img-responsive center-box"
+                     style="width:100%; height: 27%;">
+                </figure>
+                
+            </div>
+            <div class="full-reset nav-lateral-list-menu">
+                <ul class="list-unstyled">
+                    <li><a href="index.php"><i class="zmdi zmdi-home zmdi-hc-fw"></i>&nbsp;&nbsp; Inicio</a></li>
+                    <li>
+                        <div class="dropdown-menu-button"><i class="zmdi zmdi-store"></i>&nbsp;&nbsp; Productos <i class="zmdi zmdi-chevron-down pull-right zmdi-hc-fw"></i></div>
+                        <ul class="list-unstyled">
+                            <li><a href="../nuevoProducto.php"><i class="zmdi zmdi-collection-plus"></i>&nbsp;&nbsp; Agregar Nuevo Producto</a></li>
+                            <li><a href="../mostrarPro.php"><i class="zmdi zmdi-collection-text"></i>&nbsp;&nbsp; Listado de Productos</a></li>
+                            <li><a href="../precioPro.php"><i class="zmdi zmdi-money"></i>&nbsp;&nbsp; Agregar Precio a Productos</a></li>
+                            <li><a href="../mostrarPrec.php"><i class="zmdi zmdi-money-box"></i>&nbsp;&nbsp; Listado de Productos con su Precio</a></li>
+                        </ul>
+                    </li>
+                   
+                </ul>
+            </div>
+        </div>
+    </div>
+    <div class="content-page-container full-reset custom-scroll-containers">
+        <nav class="navbar-user-top full-reset">
+            <ul class="list-unstyled full-reset">
+              <hy></hy>
+                <li class="mobile-menu-button visible-xs" style="float: left !important;">
+                    <i class="zmdi zmdi-menu"></i>
+                </li>
+            </ul>
+        </nav>
+        <div class="container">
+            <div class="page-header">
+              <h1 class="all-tittles">Sistema de Almacen<small>   </small></h1>
+            </div>
+       <div class="edir">
+                
+             <div class="fila">
+	            <div class="row" style="text-align:center" >
+	                <?php if($resultado){?>
+	                <h3>Su producto fue guardado con exito</h3>
+	                <?php } else{ ?>
+	                <h3>error</h3>
+	                <?php } ?>
+	                
+	                <a href="../mostrarPro.php" class="btn btn-success">ver los productos</a>
+	            </div>
+	        </div>
+            </div>
+            
+        </div>
+        <section class="full-reset text-center" style="padding: 40px 0;">         
+        </section>
+      
+	        </div>
+	   
+   
+</body>
+</html>
