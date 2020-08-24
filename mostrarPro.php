149c1869fd29a6e8b04f42a159ae248e87824fc1
edisonquispe <edisonquispevargas@gmail.com>
2020-08-23 20:02:40 -0500

    conexion a mostrar producto

--- mostrarPro.php
+++ mostrarPro.php
@@ -1,6 +1,19 @@
 <?php
 include('link.php');
+require 'conexion.php';
+         
+$where = "";
+       
+if(!empty($_POST))
+{
+    $valor = $_POST ['campo'];
+    if(!empty($valor)){
+        $where = "WHERE nombre_producto LIKE '%$valor%'";
+    }
+}
 
+$sql = "SELECT * FROM productos $where";
+$resultado = $mysqli->query($sql);
 ?>
 <!DOCTYPE html>
 <html lang="es">
