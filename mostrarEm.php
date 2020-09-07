370e185a166f54ad35b54b7a6c1fa671d88b2010
edisonquispe <edisonquispevargas@gmail.com>
2020-09-07 11:34:26 -0500

    borrar link de referencia

--- mostrarEm.php
+++ mostrarEm.php
@@ -21,11 +21,7 @@ $resultado = $mysqli->query($sql);
     <title>Listado de Empleados</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
     
-
 </head>
 <body>
     <div class="navbar-lateral full-reset">
