0afd4dd87166288eda41c5c86d828faa56454af6
edisonquispe <edisonquispevargas@gmail.com>
2020-09-07 11:04:36 -0500

    corregir validacion del formulario de nuevo producto

--- nuevoProducto.php
+++ nuevoProducto.php
@@ -8,7 +8,9 @@ include('link.php');
     <title>Agregar Producto</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-     
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 </head>
 <body>
     <div class="navbar-lateral full-reset">
@@ -82,7 +84,7 @@ include('link.php');
 				<div class="form-group">
 					<label for="nombre" class="">Nombre del Producto</label>
 						<input type="text" class="form-control" id="nombre" name="nombre"
-						  required='required' title="Nombre del producto, Solamente letras de a-z" pattern="[a-zA-Z ]+">
+						  required='required' title="Nombre del producto, Solamente letras de a-z" pattern="[a-zA-Z./ ]+">
 					
 				</div>
 				<div class="form-group">
